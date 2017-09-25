6c182b09d492163fc35c18a51a0a25335cbca619
senaingrid <ingrid@back4app.com>
2017-09-25 17:55:19 -0300

    working project

--- BasicUserRegistration/app/src/main/java/com/back4app/basicuserregistration/MainActivity.java
+++ BasicUserRegistration/app/src/main/java/com/back4app/basicuserregistration/MainActivity.java
@@ -1,21 +1,191 @@
 package com.back4app.basicuserregistration;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.SignUpCallback;
+import com.parse.SaveCallback;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_username;
+    EditText et_password;
+
+    Button bt_login;
+    Button bt_register;
+
+    TextView message;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
+        message = (TextView) findViewById(R.id.message);
+
+        bt_login = (Button) findViewById(R.id.bt_login);
+        bt_register = (Button) findViewById(R.id.bt_register);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("8cQOhBQXiYrv5cnxVqRbUBJ53KfDIqHojw3U1N7s") //PASTE YOUR Back4App APPLICATION ID
-                .clientKey("Dt0cVr0m9TD5MY3CC85JrnwmP33fJUJI90lM6Jny") //PASTE YOUR CLIENT KEY
+                .applicationId("znPJtKs3ulIypFkuVP9nVxUpaC47qijSyqe6BQ6C") //PASTE YOUR Back4App APPLICATION ID
+                .clientKey("7m0FBbyeIuwZFpIP7ZUVRHmC3PvZt3XKHhW0Ogqs") //PASTE YOUR CLIENT KEY
                 .server("https://parseapi.back4app.com/").build()
         );
+
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                progressDialog.setMessage("Please Wait");
+                progressDialog.setTitle("Login in");
+                progressDialog.show();
+                new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                parseLogin();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                /*
+                ParseUser.logInInBackground(et_username.getText().toString(), et_password.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser parseUser, ParseException e) {
+                    if (parseUser != null) {
+                        //Login Successful
+                        //You may choose what to do or display here
+                        message.setText("Welcome "+parseUser.getUsername());
+                    } else {
+                        //Login Fail
+                        //get error by calling
+                        message.setText(e.getMessage());
+                    }
+                }
+                */
+            }
+        });
+
+
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.setMessage("Please Wait");
+                progressDialog.setTitle("Registering");
+                progressDialog.show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            parseRegister();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+                /*
+                ParseUser user = new ParseUser();
+                user.setUsername(et_username.getText().toString());
+                user.setPassword(et_password.getText().toString());
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            //Register Successful
+                            //TODO: deletar msg de teste
+                            //You may choose what to do or display here
+                            message.setText("Registered with success!");
+                        } else {
+                            //Register Fail
+                            //TODO: deletar msg de teste
+                            //get error by calling
+                            message.setText(e.getMessage());
+                        }
+                    }
+                });
+                */
+            }
+        });
+
     }
+
+    void parseLogin() {
+        ParseUser.logInInBackground(et_username.getText().toString(), et_password.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser parseUser, ParseException e) {
+                if (parseUser != null) {
+                    progressDialog.dismiss();
+                    alertDisplayer("Login Successful","Welcome "+parseUser.getUsername());
+                } else {
+                    progressDialog.dismiss();
+                    alertDisplayer("Login Failed", e.getMessage()+" Please Try Again");
+                }
+            }
+        });
+    }
+
+    void parseRegister(){
+        ParseUser user = new ParseUser();
+        user.setUsername(et_username.getText().toString());
+        user.setPassword(et_password.getText().toString());
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+
+                    progressDialog.dismiss();
+                    et_username.setText(ParseUser.getCurrentUser().getUsername());
+                    saveNewUser();
+                } else {
+                    progressDialog.dismiss();
+                    alertDisplayer("Register Fail", e.getMessage());
+                }
+            }
+        });
+    }
+
+    void saveNewUser(){
+        ParseUser user = ParseUser.getCurrentUser();
+        user.setUsername(et_username.getText().toString());
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                alertDisplayer("Register Successful Welcome", "User:" + et_username.getText().toString());
+            }
+        });
+    }
+
+    void alertDisplayer(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+    }
+
 }
