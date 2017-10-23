eef84109c1805a92168e82d785730a947fed18e7
Ingrid Sena <ingrid@back4app.com>
2017-10-23 11:00:55 -0200

    Setting global Back4App ID's

--- app/src/main/java/com/back4app/basicuserregistration/App.java
+++ app/src/main/java/com/back4app/basicuserregistration/App.java
@@ -1,5 +1,6 @@
 package com.back4app.basicuserregistration;
 
+import com.parse.Parse;
 import android.app.Application;
 
 /**
@@ -7,4 +8,9 @@ import android.app.Application;
  */
 
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this);
+    }
 }
